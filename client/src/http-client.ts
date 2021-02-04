b6d675a04aa5d0d2d38cc3d4976ff7a6da12da0e
Brad <brad@horizontwolabs.com>
2021-02-04 09:34:52 -0500

    wip

--- client/src/http-client.ts
+++ client/src/http-client.ts
@@ -13,13 +13,7 @@ export class HttpClient {
     if (!path.startsWith('/')) {
       path = `/${path}`;
     }
-
     const headers = token ? { api_session_token: token } : null;
-
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    // },
-
     const options = {
       hostname,
       path,
@@ -27,7 +21,7 @@ export class HttpClient {
       method,
     } as RequestOptions;
 
-    console.log('http client : request :', options);
+    // console.log('http client : request :', options);
 
     return new Promise((resolve, reject) => {
       const req = request(options, res => {
@@ -38,9 +32,7 @@ export class HttpClient {
 
         const chunks: Buffer[] = [];
 
-        res.on('data', data => {
-          chunks.push(data);
-        });
+        res.on('data', data => chunks.push(data));
 
         res.on('end', () => {
           let data: T;
@@ -50,7 +42,6 @@ export class HttpClient {
             if (isJSON) {
               data = JSON.parse(body.toString());
             }
-            // data = JSON.parse(raw);
           } catch (err) {
             console.log('http : error parsing data as json :', data!);
             resolve(data!);
@@ -83,6 +74,8 @@ export class HttpClient {
 
   /**
    * Hits API to get a session token, registers it on class
+   * @example:
+   * curl https://app-dev.emtrey.io/api/user/generate-raw-api-session
    */
   public async generateSessionToken(): Promise<string> {
     const path = '/api/user/generate-raw-api-session';
@@ -100,17 +93,32 @@ export class HttpClient {
     return this.token;
   }
 
-  public async getUser<T = User>(): Promise<T> {
-    const url = '/api/user';
-    return this.get<T>(url);
+  public async getUser<T = User>() {
+    return this.get<T>('/api/user');
   }
 
   /**
-   * Post run through results.
+   * @example:
+   * curl -H "api_session_token: haDZ3hKdX46sbaeTXVkHzLZ-gfeEp6IoNOqHmdGaXfDa7d0K4jEprWo61-58" \
+      -d "name=project2&github_url=foo&org_id=1" \
+      -X POST https://app-dev.emtrey.io/api/project
    */
-  public async postResults() {
-    const url = 'api/run-through';
-    return this.post(url, {});
+  public async createProject<T = unknown>() {
+    const params = {
+      name: 'foo',
+      github_url: null,
+      org_id: null,
+    };
+    return this.post<T>('api/project', params);
+  }
+
+  public async postResults<T = unknown>() {
+    const params = {
+      branch: 'branch',
+      commit: 'baz',
+      project_id: null,
+    };
+    return this.post<T>('api/run-through', params);
     // curl -H "api_session_token: fYBIu4nP85qE-0xW0BhyooG5EdbPrUZ6QcwnkM3JCH3Ea30sO9unMNOOThsWl" -d "branch=feature/cool&commit=983u92f9ejwio3j9efw&project_id=2" -X POST https://early-testing-emtrey.herokuapp.com/api/run-through
   }
 }
