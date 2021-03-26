13a0777591b1886fe4c5e88ddd7cc61473603f29
Max Dignan <maxdignan@gmail.com>
2021-03-26 11:26:15 -0400

    red bg

--- src/Project.js
+++ src/Project.js
@@ -5,10 +5,15 @@ class Project extends React.Component {
     return (
       <div className="project">
         <h1>Project!</h1>
+        <div style={styleSheet}>Bunch of red text</div>
         <p>{this.props.params.projectId} + 1234567901 + jfkdlsjafkdsjafjfkdsjafkldjsaklfdjskalfjdsaklfjdskla jfkdlsjafkldsjaklfdjsaklfjdskuivhfiodhskljvdsklajf</p>
       </div>
     );
   }
+}
+
+const styleSheet = {
+  backgroundColor: 'red',
 }
 
 export default Project;
