b784318480867b0662313e5efacaf7ff9a8a0919
Max Dignan <maxdignan@gmail.com>
2021-04-28 16:20:58 -0400

    Should have a few unresolveds

--- src/Project.js
+++ src/Project.js
@@ -5,7 +5,7 @@ class Project extends React.Component {
     return (
       <div className="project">
         <h1>Project!</h1>
-        <p>{this.props.params.projectId}</p>
+        <p>{this.props.params.projectId} should be unresolved</p>
       </div>
     );
   }
